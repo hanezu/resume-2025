9524326ef6712e39f1136c4841578e0983dfc6b1
hanezu <13895824+hanezu@users.noreply.github.com>
2025-06-28 12:32:01 -0400

    update color

--- edit-me/config/resume-config.ts
+++ edit-me/config/resume-config.ts
@@ -2,8 +2,8 @@ import { AccentColor, ColorMode, NeutralColor } from '@strum/colors';
 import { ResumeConfig, ThemeSetting } from '../types/config';
 
 export const resumeConfig: ResumeConfig = {
-  accentColor: AccentColor.Blue,
-  neutralColor: NeutralColor.Slate,
+  accentColor: AccentColor.Cyan,
+  neutralColor: NeutralColor.Sand,
   appTheme: ThemeSetting.System,
   imageTheme: ColorMode.Light,
   pdfTheme: ColorMode.Light,
